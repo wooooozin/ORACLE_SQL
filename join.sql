4e28b249eedc1c6792e2e32f966f28522a1a8870
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-04-03 05:14:18 +0900

    수정

--- join.sql
+++ join.sql
@@ -21,9 +21,47 @@ FROM emp e, dept d
 WHERE e.deptno = d.deptno;
 
 -- DALLAS에 근무하고 있는 사원들의 사원번호, 이름, 직무를 가져온다.
-SELECT e.empno, e.ename, e.job
+SELECT e.empno, e.ename, e.job, d.loc
 FROM emp e, dept d
 WHERE e.deptno = d.deptno
 AND d.loc = 'DALLAS';
+
+-- SALES 부서에 근무하고 있는 사원들의 급여 평균
+SELECT AVG(sal)
+FROM emp e, dept d
+WHERE e.deptno = d.deptno 
+AND d.deptno = 30;
+
+-- 1982년에 입사한 사원들의 사원번호, 이름, 입사일, 근무부서 이름
+SELECT e.empno,  e.ename, e.hiredate, d.dname
+FROM emp e, dept d
+WHERE e.deptno = d.deptno
+AND e.hiredate between '1982/01/01' AND '1982/12/31';
+
+-- 각 사원들의 사원번호, 이름, 급여, 급여 등급
+SELECT e.empno, e.ename, e.sal, s.grade
+FROM emp e, salgrade s
+WHERE e.sal between s.losal AND s.hisal;
+
+-- salse 부서에 근무하고 있는 사원의 사원번호, 이름, 급여등급
+SELECT e.empno, e.ename, s.grade, d.dname
+FROM emp e, dept d, salgrade s
+WHERE e.deptno = d.deptno
+AND d.dname = 'SALES'
+AND e.sal between s.losal AND s.hisal;
+
+-- 각 급여 등급별 급여의 총합과 평균, 사원의 수, 최대급여, 최소 급여
+SELECT s.grade, SUM(e.sal), TRUNC(AVG(e.sal)), COUNT(e.sal), MAX(e.sal), MIN(e.sal)
+FROM emp e, salgrade s
+WHERE e.sal BETWEEN s.losal AND s.hisal
+GROUP BY s.grade;
+
+-- 급여 등급이 4등급인 사원들의 번호, 이름, 급여, 부서이름, 지역
+SELECT s.grade, e.empno, e.ename, e.sal, d.dname, d.loc
+FROM emp e, salgrade s, dept d
+WHERE e.sal BETWEEN s.losal AND s.hisal
+AND e.deptno = d.deptno
+AND s.grade = 4;
+
 
 
--- self-outer-join.sql
+++ self-outer-join.sql
@@ -17,8 +17,23 @@ WHERE h.ename = 'FORD'
 AND e.mgr = h.empno;
 
 -- SMITH 사원의 직속상관과 동일한 직무를 가지고 있 사원들의 사원번호, 이름 , 직무 가져오기
-SELECT e3.ename, e3.empno, e3.job
+SELECT e3.ename, e3.empno, e3.job, e1.mgr, e2.ename
 FROM emp e1, emp e2, emp e3
 WHERE e1.ename = 'SMITH'
-AND e1.mgr = e2.mgr
+AND e1.mgr = e2.empno
 AND e2.job = e3.job;
+
+
+-- OUTER JOIN
+-- 조인 조건에 해당하지 않아 결과에 포함되지 않는 로우까지 가져오는 조인
+
+-- 각 사원의 이름, 사원번호, 상사이름, 단 상사이름이 없는 사원도 가져온다
+SELECT e1.ename, e1.empno, e2.ename
+FROM emp e1, emp e2
+WHERE e1.mgr = e2.empno(+)
+;
+
+-- 모든 부서의 소속사원의 근무부서명, 사원번호, 사원이름, 급여
+SELECT d.deptno, d.dname, e.empno, e.ename, e.sal
+FROM dept d, emp e
+WHERE d.deptno = e.deptno(+);;
